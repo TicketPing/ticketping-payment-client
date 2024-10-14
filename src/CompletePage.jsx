7bf59ceb95f959486923a5276146284653d25589
ryehwa <rhk415@gmail.com>
2024-10-14 18:56:46 +0900

    modify initialState

--- src/CompletePage.jsx
+++ src/CompletePage.jsx
@@ -47,7 +47,7 @@ const STATUS_CONTENT_MAP = {
 export default function CompletePage({pid}) {
   const stripe = useStripe();
 
-  const [status, setStatus] = useState("default");
+  const [status, setStatus] = useState("processing");
   const [intentId, setIntentId] = useState(null);
 
   useEffect(() => {

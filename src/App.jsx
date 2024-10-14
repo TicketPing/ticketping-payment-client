80b015ef8edb5a059fbaa1aaf9a3fc622793c29b
ryehwa <rhk415@gmail.com>
2024-10-14 18:26:33 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from "react";
+import {loadStripe} from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import CompletePage from "./CompletePage";
+import Modal from "./Modal";
+import "./App.css";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+const stripePromise = loadStripe("pk_test_51Q801tRpxgEVE539pBARC3D84A8MPZskWP4cyVuXZZOWGDVFmD2YF7pfZArK7jYymrTSZ4aGE2hlxKe0LLUUaHrI00NC71Qgaz");
+
+export default function App() {
+
+    const [clientSecret, setClientSecret] = useState("");
+    const [dpmCheckerLink, setDpmCheckerLink] = useState("");
+    const [isModalOpen, setIsModalOpen] = useState(true);
+    const [pid, setPid] = useState("");
+    const appearance = {
+        theme: 'stripe',
+    };
+    // Enable the skeleton loader UI for optimal loading.
+    const loader = 'auto';
+    const path = window.location.pathname;
+
+    useEffect(() => {
+        if (path === '/complete') {
+            const search = window.location.search
+            setIsModalOpen(false)
+            setClientSecret(new URLSearchParams(search).get('payment_intent_client_secret'))
+            setPid(new URLSearchParams(search).get('payment_intent'));
+        }
+    }, []);
+    return (
+        <Router>
+            <div className="App">
+                {isModalOpen && <Modal modal={{setIsModalOpen, setClientSecret, setDpmCheckerLink}} /> }
+                {clientSecret && (
+                    <Elements options={{clientSecret, appearance, loader}} stripe={stripePromise}>
+                        <Routes>
+                            <Route path="/client" element={<CheckoutForm dpmCheckerLink={dpmCheckerLink}/>}/>
+                            <Route path="/complete" element={<CompletePage pid={pid}/>}/>
+                        </Routes>
+                    </Elements>
+                )}
+            </div>
+        </Router>
+    )
+}
---
+++ src/Modal.jsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+
+export default function Modal({modal}) {
+    const {setIsModalOpen, setClientSecret, setDpmCheckerLink} = modal;
+    const [order, setOrder] = useState("")
+    const handleModalSubmit = () => {
+        fetch(`http://localhost:8080/api/v1/payments/faec94ca-8583-4380-b0b5-6c6458abb220`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setIsModalOpen(false);
+                setClientSecret(data.data.clientSecret);
+                setDpmCheckerLink(data.data.dpmCheckerLink);
+            });
+    };
+
+
+    return <>
+        <div className="modal">
+            <div className="modal-content">
+                <h2>상품 ID 입력</h2>
+                <input
+                    type="text"
+                    placeholder="상품 ID를 입력하세요"
+                    // faec94ca-8583-4380-b0b5-6c6458abb220
+                    value={order}
+                    onChange={(e) => setOrder(e.target.value)}
+                />
+                <button onClick={handleModalSubmit}>확인</button>
+            </div>
+        </div>
+    </>
+}
